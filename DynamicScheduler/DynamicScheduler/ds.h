64b54429e087537c3194a4c670500d0d4168d218
wtkersting <wtkersting@gmail.com>
2016-11-23 16:03:40 -0500

    Update ds.h

--- DynamicScheduler/DynamicScheduler/ds.h
+++ DynamicScheduler/DynamicScheduler/ds.h
@@ -9,6 +9,8 @@
 #include <vector>
 #include <math.h>
 
+#include "structs.h"
+
 using namespace std;
 
 class dynamic_scheduler
@@ -16,46 +18,40 @@ class dynamic_scheduler
 private:
 	vector<IQ> iq;		// Issue Queue
 	vector<ROB> rob;	// ReOrder Buffer
-	vector<ARF> r;		// # of Architectual Registers
+	vector<int> r;		// # of Architectual Registers - ARF
 	vector<RMT> rmt;	// # of Architectual Registers
 
-	std::ifstream infile;
-	std::string line;
+	int h, t;	// Head and tail pointers for ROB
 
-	vector<std::string> splt_str;
-	std::string pc;
+	std::ifstream _infile;
+	std::string _line;
 
-	int op;
-	int dst;
-	int src1;
-	int src2;
+	vector<std::string> _splt_str;
+	std::string _pc;
+
+	vector<bndl> fe;
+	vector<bndl> de;
+	vector<bndl> rn;
+	vector<bndl> rr;
+	vector<bndl> di;
+	vector<bndl> is;
+	vector<bndl> ex;
+	vector<bndl> wb;
+	vector<bndl> rt;
+
+	int _pl;	// # instructions in the pipeline
+
+	bool isRobFull();
+	bool isBndlEmpty(vector<bndl> b);
+	bool isBndlFull(vector<bndl> b);
 
 public:
-	long rob_size;
-	long iq_size;
-	long width;
-	long cache_size;
-	long p;
+	long rob_size, iq_size, width, cache_size, p;
 	std::string tracefile;
 
-	long cycle;
+	long _cycle;
 
-	dynamic_scheduler(long r, long i, long w, long c, long P, std::string tf)
-	{
-		rob_size = r;
-		iq_size = i;
-		width = w;
-		cache_size = c;
-		p = P;
-		tracefile = tf;
-
-		this->iq.resize(iq_size);
-		this->rob.resize(rob_size);
-		this->r.resize(67);
-		this->rmt.resize(67);
-
-		this->cycle = 0;
-	}
+	dynamic_scheduler(long r, long i, long w, long c, long P, std::string tf);
 
 	void retire();
 	void writeback();
@@ -68,8 +64,6 @@ public:
 	void fetch();
 
 	bool advance_cycle();
-
-	void initialize();
 
 	void setRobSize(long rob);
 	void setIQSize(long iq);
@@ -86,46 +80,3 @@ public:
 	std::string getTF(void);
 };
 
-class pipeline
-{
-	public:
-		int *de;
-		int *rn;
-		int *rr;
-		int *di;
-		int *iq;
-		int *ex_lst;
-		int *wb;
-		int *rob;
-};
-
-struct ROB
-{
-	int val;
-	int dst;
-	bool rdy;
-	bool exc;
-	bool mis;
-	std::string pc;
-};
-
-struct RMT
-{
-	int val;
-	std::string tag;
-};
-
-struct ARF
-{
-	int val;
-};
-
-struct IQ
-{
-	bool val;
-	std::string dst_tag;
-	bool s1_rdy;
-	std::string s1_val;
-	bool s2_rdy;
-	std::string s2_val;
-};
